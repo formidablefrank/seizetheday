ce3048c6bc8e09934a97f3800e63a1d2c88fc063
Jerome Indefenzo <jerome.indefenzo@upd.edu.ph>
2015-08-09 00:25:16 +0800

    fix visual issue with the Identify page question links

--- application/views/identify.php
+++ application/views/identify.php
@@ -39,19 +39,13 @@
 <div class="ui stackable centered grid container">
 
 	<div class="ui massive buttons">
-		<!-- <div class="seven wide column"> -->
-			<button class="ui positive button"><a href="<?php echo base_url('home/identify/' . $question->question_id . '/' . $choices[0]->choice_id) ?>">
+			<a class="ui positive button" href="<?php echo base_url('home/identify/' . $question->question_id . '/' . $choices[0]->choice_id) ?>">
 				<?php echo $choices[0]->choice_text; ?>
-			</a></button>
-		<!-- </div> -->
-		<!-- <div class="two wide column"> -->
+			</a>
 			<div class="or"></div>
-		<!-- </div> -->
-		<!-- <div class="seven wide column"> -->
-			<button class="ui negative button"><a href="<?php echo base_url('home/identify/' . $question->question_id . '/' . $choices[1]->choice_id) ?>">
+			<a class="ui negative button" href="<?php echo base_url('home/identify/' . $question->question_id . '/' . $choices[1]->choice_id) ?>">
 				<?php echo $choices[1]->choice_text; ?>
-			</a></button>
-		<!-- </div> -->
+			</a>
 
 	</div>
 
