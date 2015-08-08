3592f2f95d2de6961c013dd5c5b46c1e0d70ce4d
Dyosa Ganda <mariaaprilrose@gmail.com>
2015-08-08 21:17:50 +0800

    Centered question in identify

--- application/views/identify.php
+++ application/views/identify.php
@@ -1,4 +1,8 @@
-<h2><?php echo $question->question_text; ?></h2>
+<h2 class="ui center aligned header">
+	<?php echo $question->question_text; ?>
+</h2>
+
+
 <?php foreach ($choices as $choice) { ?>
     <button class=""><a href="<?php echo base_url('home/identify/' . $question->question_id . '/' . $choice->choice_id) ?>">
         <?php echo $choice->choice_text; ?>
