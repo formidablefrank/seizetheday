db6cdbf6c25aeff76ef6e6c64f5d63a00f6ddce4
unknown <frances.calceta@gmail.com>
2015-08-08 23:20:43 +0800

    questions lookup table

--- application/models/question.php
+++ application/models/question.php
@@ -23,9 +23,31 @@ class Question extends CI_Model{
     }
 
     function evalQuestion($qnum, $ans){
-        if($qnum == 0 && $ans == 0) {$qnum = 1;}
-        if($qnum == 1 && $ans == 1) {$qnum = 2;}
-        return $qnum;
+        $lookup = [ [1, 0,0,0,0],
+                    [0, 3, "healthy", 0, 0],
+                    [0, 0, 0, 7, 12],
+                    [0, 6, 6, 0, 0],
+                    [0, 5, "healthy",0,0],
+                    [0, "HIV", 6, 0, 0],
+                    [0, 2, 15, 0, 0],
+                    [0, "chlam", 8, 0, 0],
+                    [0, 9, 10, 0, 0],
+                    [0, "gon", 11, 0, 0],
+                    [0, "chlam","dead end",0,0],
+                    [0, "tricho", 10, 0, 0],
+                    [0, 13, 14, 0, 0],
+                    [0, "gon", 10, 0, 0],
+                    [0, 13, 14, 3, 4],
+                    [0, 16, 3, 0, 0],
+                    [0, 17, 19, 0, 0],
+                    [0, "scabies", 18, 0, 0],
+                    [0, "scabies", "pubic lice", 0,0],
+                    [0, "yeast", 20, 0, 0],
+                    [0, 21, "dead end", 0, 0],
+                    [0, "herpes", 22, 0, 0],
+                    [0, "syph", "warts", 0, 0]
+                  ];
+        return $lookup[$qnum][$ans];
     }
 }
 ?>
