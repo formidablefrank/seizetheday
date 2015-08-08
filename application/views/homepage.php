5add33f21a9b4239c3dfb7ef9b4dd4a96ca67028
Jerome Indefenzo <jerome.indefenzo@upd.edu.ph>
2015-08-08 20:55:33 +0800

    various homepage tweaks

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -1,70 +1,77 @@
 
-<!-- application homepage branding -->
-<div class="ui container">
-    <h1> <i class="heart icon"></i> Seize the Day</h1>
-</div>
+<div id="homepage-masthead">
+	<!-- application homepage branding -->
+	<div class="ui large secondary menu">
+		<div class="ui container">
+			<div class="item">
+				<h1><i class="heart icon"></i> Seize the Day</h1>
+			</div>
+			<div class="item">
+			</div>
+		</div>
+	</div>
 
-<!-- feature quick links -->
-<div class="ui stackable centered grid container">
-    <div class="five wide column">
-    	<div class="ui centered grid container">
-    		<div class="fourteen wide column">
-				<img class="ui fluid image" src="<?php echo base_url('img/connect.svg'); ?>">
-				<button class="fluid ui huge button">Connect</button>
-    		</div>
-    	</div>
-    </div>
-    <div class="five wide column">
-            <div class="ui card">
-                <div class="ui fade reveal image">
-                    <img src="img/Forever.png" class="visible content">
-                    <img src="/images/avatar/large/elliot.jpg" class="hidden content">
-                </div>
-                <div class="content">
-                    <div class="ui center aligned container">
-                        <a class="header">Seek</a>
-                    </div>
-                </div>
-            </div>
-    </div>
-    <div class="five wide column">
-            <div class="ui card">
-                <div class="ui fade reveal image">
-                    <img src="img/Forever.png" class="visible content">
-                    <img src="/images/avatar/large/elliot.jpg" class="hidden content">
-                </div>
-                <div class="content">
-                    <div class="ui center aligned container">
-                        <a class="header">Identify</a>
-                    </div>
-                </div>
-            </div>
-    </div>
+	<!-- feature quick links -->
+	<div id="quick-links" class="ui stackable centered grid container">
+		<div class="five wide column">
+			<div class="ui centered grid container">
+				<div class="fourteen wide column">
+					<img class="ui fluid image" src="<?php echo base_url('img/connect.svg'); ?>">
+					<button class="fluid ui huge black button">Connect</button>
+				</div>
+			</div>
+		</div>
+		<div class="five wide column">
+				<div class="ui card">
+					<div class="ui fade reveal image">
+						<img src="img/Forever.png" class="visible content">
+						<img src="/images/avatar/large/elliot.jpg" class="hidden content">
+					</div>
+					<div class="content">
+						<div class="ui center aligned container">
+							<a class="header">Seek</a>
+						</div>
+					</div>
+				</div>
+		</div>
+		<div class="five wide column">
+				<div class="ui card">
+					<div class="ui fade reveal image">
+						<img src="img/Forever.png" class="visible content">
+						<img src="/images/avatar/large/elliot.jpg" class="hidden content">
+					</div>
+					<div class="content">
+						<div class="ui center aligned container">
+							<a class="header">Identify</a>
+						</div>
+					</div>
+				</div>
+		</div>
+	</div>
 </div>
 
 <!-- feature overview and description -->
 <div class="ui fluid container">
-    <div class="ui divider"></div>
-    <div class="ui container">
-        <h2>Feature 1</h2>
-        <p>Overview</p>
-    </div>
+	<div class="ui container">
+		<h2>Feature 1</h2>
+		<p>Overview</p>
+	</div>
 </div>
 
 <div class="ui fluid container">
-    <div class="ui container">
-        <div class="ui divider"></div>
-        <h2>Feature 2</h2>
-        <p>Overview</p>
-    </div>
+	<div class="ui container">
+		<div class="ui divider"></div>
+		<h2>Feature 2</h2>
+		<p>Overview</p>
+	</div>
 </div>
 
 <div class="ui fluid container">
-    <div class="ui container">
-        <div class="ui divider"></div>
-        <h2>Feature 3</h2>
-        <p>Overview</p>
-    </div>
+	<div class="ui container">
+		<div class="ui divider"></div>
+		<h2>Feature 3</h2>
+		<p>Overview</p>
+	</div>
 </div>
 
 <!-- // temporarily comment out for faster local loading
