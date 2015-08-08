75d00070d31b18741e9e63b18f0d0354104e75ff
Joshua Frankie Rayo <kean.franco07@gmail.com>
2015-08-09 00:14:36 +0800

    identify backend feature complete.

--- application/models/question.php
+++ application/models/question.php
@@ -9,13 +9,13 @@ class Question extends CI_Model{
         parent::__construct();
     }
 
-    function getQuestion($qnum){
+    function getQuestion($qid){
         $this->db->limit(1);
-        return $this->db->get_where('questions', array('question_id' => $qnum));
+        return $this->db->get_where('questions', array('question_id' => $qid));
     }
 
-    function getChoices($qnum){
-        return $this->db->query('SELECT DISTINCT question_id, choice_id, choice_text FROM questions_choices NATURAL JOIN choices WHERE question_id = 1');
+    function getChoices($qid){
+        return $this->db->query("SELECT DISTINCT question_id, choice_id, choice_text FROM questions_choices NATURAL JOIN choices WHERE question_id = '$qid';");
     }
 
     function putAnswer($qid, $ans, $userid){
@@ -25,25 +25,25 @@ class Question extends CI_Model{
     function evalQuestion($qnum, $ans){
         $lookup = [ [1, 0,0,0,0],
                     [0, 3, "healthy", 0, 0],
-                    [0, 0, 0, 7, 12],
+                    [0, 0, 0, 12, 7],
                     [0, 6, 6, 0, 0],
                     [0, 5, "healthy",0,0],
                     [0, "HIV", 6, 0, 0],
                     [0, 2, 15, 0, 0],
-                    [0, "chlam", 8, 0, 0],
+                    [0, "chlamydia", 8, 0, 0],
                     [0, 9, 10, 0, 0],
-                    [0, "gon", 11, 0, 0],
-                    [0, "chlam","dead end",0,0],
+                    [0, "gonorrhea", 11, 0, 0],
+                    [0, "chlamydia","consult",0,0],
                     [0, "tricho", 10, 0, 0],
                     [0, 13, 14, 0, 0],
-                    [0, "gon", 10, 0, 0],
+                    [0, "gonorrhea", 10, 0, 0],
                     [0, 13, 14, 3, 4],
                     [0, 16, 3, 0, 0],
                     [0, 17, 19, 0, 0],
                     [0, "scabies", 18, 0, 0],
                     [0, "scabies", "pubic lice", 0,0],
                     [0, "yeast", 20, 0, 0],
-                    [0, 21, "dead end", 0, 0],
+                    [0, 21, "consult", 0, 0],
                     [0, "herpes", 22, 0, 0],
                     [0, "syph", "warts", 0, 0]
                   ];
--- application/views/identify.php
+++ application/views/identify.php
@@ -5,27 +5,22 @@
 
 <!-- Display yes and no buttons -->
 <div class="ui stackable centered grid container">
-	<!--
-	<?php foreach ($choices as $choice) { ?>
-		<div class="eight wide column">
-			<button class="fluid ui button"><a href="<?php echo base_url('home/identify/' . $question->question_id . '/' . $choice->choice_id) ?>">
-				<?php echo $choice->choice_text; ?>
-			</a></button>
-		</div>
-	<?php } ?>
-	-->
 
 	<div class="ui massive buttons">
 		<!-- <div class="seven wide column"> -->
-			<button class="ui positive button"><?php echo $choices[0]->choice_text; ?></button>
+			<button class="ui positive button"><a href="<?php echo base_url('home/identify/' . $question->question_id . '/' . $choices[0]->choice_id) ?>">
+				<?php echo $choices[0]->choice_text; ?>
+			</a></button>
 		<!-- </div> -->
 		<!-- <div class="two wide column"> -->
 			<div class="or"></div>
 		<!-- </div> -->
 		<!-- <div class="seven wide column"> -->
-			<button class="ui negative button"><?php echo $choices[1]->choice_text; ?></button>
+			<button class="ui negative button"><a href="<?php echo base_url('home/identify/' . $question->question_id . '/' . $choices[1]->choice_id) ?>">
+				<?php echo $choices[1]->choice_text; ?>
+			</a></button>
 		<!-- </div> -->
-							
+
 	</div>
 
 </div>
