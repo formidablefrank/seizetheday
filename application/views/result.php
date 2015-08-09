7d6066a30db25cbd408ae31380148dac0e410db9
unknown <frances.calceta@gmail.com>
2015-08-09 09:16:06 +0800

    modified descripions on result page

--- application/views/result.php
+++ application/views/result.php
@@ -41,16 +41,16 @@
 <!-- set description based on result -->
 <?php
 if($result == 'consult'){ $desc = 'Consult a doctor right away!'; }
-else if($result == 'healthy'){ $desc = 'You are healthy. No need to worry.'; }
-else if($result == 'chlamydia'){ $desc = 'Consult a doctor right away!'; }
-else if($result == 'HIV'){ $desc = 'Description'; }
-else if($result == 'gonorrhea'){ $desc = 'Consult a doctor right away!'; }
-else if($result == 'scabies'){ $desc = 'Consult a doctor right away!'; }
-else if($result == 'pubic lice'){ $desc = 'Consult a doctor right away!'; }
-else if($result == 'yeast'){ $desc = 'Consult a doctor right away!'; }
-else if($result == 'herpes'){ $desc = 'Consult a doctor right away!'; }
-else if($result == 'syphilis'){ $desc = 'Consult a doctor right away!'; }
-else if($result == 'warts'){ $desc = 'Consult a doctor right away!'; }
+else if($result == 'healthy'){ $desc = 'You are healthy! However, if you have any worries, have yourself checked.'; }
+else if($result == 'chlamydia'){ $desc = "Chlamydia is one of the most common STD's. It is also easily cured. Visit a clinic!"; }
+else if($result == 'HIV'){ $desc = 'It is possible that you have HIV. Have yourself checked!'; }
+else if($result == 'gonorrhea'){ $desc = 'If left untreated, gonorrhea can cause infertility. Consult a doctor right away!'; }
+else if($result == 'scabies'){ $desc = 'Scabies can and will spread to other parts of your skin if left untreated. Go to a doctor.'; }
+else if($result == 'pubic lice'){ $desc = 'Getting rid of your pubic hair is not enough. Get checked! Special creams may be perscribed.'; }
+else if($result == 'yeast'){ $desc = 'People who eat a lot of sugar have higher risks of getting yeast infections. Eating yogurt and acidic food may help, but it is best to visit a doctor if you have worries.'; }
+else if($result == 'herpes'){ $desc = 'It is possible that you have herpes. Go to a doctor! It is very contagious.'; }
+else if($result == 'syphilis'){ $desc = 'Syphilis can lead to death if untreated. Have it checked now!'; }
+else if($result == 'warts'){ $desc = 'You have genital warts! They are causes by the Human Pappiloma Virus (HPV). Have it checked!'; }
 ?>
 
 <!-- display result -->
