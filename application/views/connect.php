14432496839d86523f236da7ccac13ef250800ce
Dyosa Ganda <mariaaprilrose@gmail.com>
2015-08-09 10:03:35 +0800

    fixed UI of modal of Connect page

--- application/views/connect.php
+++ application/views/connect.php
@@ -74,21 +74,27 @@
 	<button class="ui button" id="talk">Talk to our partner doctors!</button>
 	<p>And schedule an appointment with them.</p>
 
-	<div class="ui modal">
+	<div class="ui small modal">
 		<i class="close icon"></i>
 		<div class="header">
 			Please provide your information and we will send you a text message for your appointment details.
 		</div>
 		<div class="content">
+			<style type="text/css">
+			.label {
+			    width:150px;
+			    display: inline-block;
+			}
+			</style>
 			<?php echo form_open(base_url('home/appointment'), array('method' => 'POST')); ?>
 			<div class="ui form">
-				<div class="field">
-					<label>Name</label>
-					<input type="text" name="name">
+				<div class="ui labeled input" style="padding: 10px 0px;">
+					<div class="ui large label">Name</div>
+					<input type="text" name="name" placeholder="Juan dela Cruz">
 				</div>
-				<div class="field">
-					<label>Mobile Number</label>
-					<input type="text" name="number">
+				<div class="ui labeled input" style="padding: 10px 0px;">
+					<div class="ui large label">Mobile Number</div>
+					<input type="text" name="number" placeholder="639XXXXXXXXX">
 				</div>
 			</div>
 		</div>
